3c9fdf61dc9f625026d7adc84ba93e549d4329d7
Suphakit P <me@suphakit.net>
2023-09-17 03:42:02 +0700

    feat<components>: added Footer

---
+++ src/modules/components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = `<div class="footer">
+  <p>
+    made w/ 🤍 by <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
+  </p>
+
+  <div>
+    <p class="left">
+      &copy; 2023 <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
+    </p>
+    <p class="right">
+      <a href="https://github.com/katzEco/katz-gallery" target="_blank">
+        <i class="fa-brands fa-github fa-xl"></i>
+      </a>
+    </p>
+  </div>
+</div>`
+
+module.exports = Footer
