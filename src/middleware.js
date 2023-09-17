0b4ad8ce8ec8e33052d0b1e4ebdcec0876c795e8
Suphakit P <me@suphakit.net>
2023-09-17 20:12:05 +0700

    feat<middleware>: setting up authenicate middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,23 @@
+const bodyParser = require('body-parser')
+const sessions = require('express-session')
+const cookieParser = require('cookie-parser')
+
 function Middleware(app, exp, root) {
   app.use('/static', exp.static(root + '/static'))
+  app.use(exp.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+
+  app.use(cookieParser())
+  app.use(
+    sessions({
+      secret: process.env.secret,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+      },
+      resave: false,
+    })
+  )
 }
 
 module.exports = Middleware
