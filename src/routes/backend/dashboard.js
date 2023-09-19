3d23d98aeff3fef81b360115932e7ac0becd2f72
Suphakit P <me@suphakit.net>
2023-09-19 13:41:49 +0700

    feat<backend>: added counter

--- src/routes/backend/dashboard.js
+++ src/routes/backend/dashboard.js
@@ -4,7 +4,11 @@ const album = require('./pages/album')
 const cosplayer = require('./pages/cosplayer')
 const image = require('./pages/image')
 
-function dashboard() {
+const cosModel = require('../../modules/mongo/schemas/cosplayer')
+const albModel = require('../../modules/mongo/schemas/album')
+const imgModel = require('../../modules/mongo/schemas/item')
+
+async function dashboard() {
   const head = `<title>Backend | ${process.env.web_title}</title>
 <style>
   .main-container {
@@ -28,33 +32,52 @@ function dashboard() {
   }
 </style>`
 
-  const body = `<div class="main-container">
-  <a href="/backend/album">
-    <button>
-      Album
-    </button>
-  </a>
-
-  <a href="/backend/cosplayer">
-    <button>
-      Cosplayer
-    </button>
-  </a>
-
-  <a href="/backend/image">
-    <button>
-      Images
-    </button>
-  </a>
+  const body = `<div class="hover-container">
+  <div class="main-container">
+    <a href="/backend/cosplayer">
+      <button>
+        Cosplayer
+      </button>
+    </a>
+    
+    <a href="/backend/album">
+      <button>
+        Album
+      </button>
+    </a>
+  
+    <a href="/backend/image">
+      <button>
+        Images
+      </button>
+    </a>
+  </div>
+  <div class="main-container">
+    <div style="width: 100%; text-align: center;">
+      <h1>
+        <u>Cosplayer Count</u> : ${(await cosModel.find()).length}
+      </h1>
+    </div> 
+    <div style="width: 100%; text-align: center;">
+      <h1>
+        <u>Album Count</u> : ${(await albModel.find()).length}
+      </h1>
+    </div>
+    <div style="width: 100%; text-align: center;">
+      <h1>
+        <u>Image Count</u> : ${(await imgModel.find()).length}
+      </h1>
+    </div>
+  </div> 
 </div>`
 
   return Layout(head, body, true)
 }
 
 async function backendDashBoard(app) {
-  app.get('/backend/dashboard', (req, res) => {
+  app.get('/backend/dashboard', async (req, res) => {
     if (req.session.loggedIn) {
-      res.send(dashboard())
+      res.send(await dashboard())
     } else {
       res.redirect('/backend')
     }
