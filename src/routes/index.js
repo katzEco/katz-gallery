dfb2e131206fb6c990b6b542512ac9922f83277f
Suphakit P <me@suphakit.net>
2023-09-20 17:41:37 +0700

    feat<album>: listing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,58 @@
 const Layout = require('./_Layout')
+const albumSlug = require('./slug')
+
+const album = require('../modules/mongo/schemas/album')
+const cosplayer = require('../modules/mongo/schemas/cosplayer')
+const aboutFunction = require('./about')
 
 async function IndexComponents() {
-  let head = `<title>Index | ${process.env.web_title}</title>`
+  const albData = await album.find().exec()
+  let dataset
+
+  for (alb of albData) {
+    const temp = `<tr>
+  <td>
+    <a href="/albums/${alb._id}">
+      ${(await cosplayer.findOne({ _id: alb.cpID })).cpName} - ${alb.albumName}
+    </a>
+  </td>
+  <td>
+    ${alb.date}
+  </td>
+</tr>`
+
+    if (dataset == undefined) {
+      dataset = temp
+    } else {
+      dataset += temp
+    }
+  }
+
+  let head = `<title>Index | ${process.env.web_title}</title>
+
+<style>
+  table {
+    width: 80%;
+    margin: 0 auto;
+  }
+
+  table th {
+    text-align: center;
+  }
+</style>`
+
   let body = `<div class="mainContainer">
-  
+  <table>
+    <tr>
+      <th>
+        Album Name
+      </th>
+      <th>
+        Date
+      </th>
+    </tr>
+    ${dataset}
+  </table>
 </div>`
 
   return Layout(head, body, 'index')
@@ -13,6 +62,9 @@ async function IndexPage(app) {
   app.get('/', async (req, res) => {
     res.send(await IndexComponents())
   })
+
+  await albumSlug(app)
+  await aboutFunction(app)
 }
 
 module.exports = IndexPage
---
+++ src/routes/slug.js
@@ -0,0 +1,132 @@
+const Layout = require('./_Layout')
+
+const cpModel = require('../modules/mongo/schemas/cosplayer')
+const abModel = require('../modules/mongo/schemas/album')
+const imgModel = require('../modules/mongo/schemas/item')
+
+async function albumPage(slug) {
+  const imgList = await imgModel
+    .find({ albumID: slug })
+    .sort({ _id: 'desc' })
+    .exec()
+  const album = await abModel.findOne({ _id: slug }).exec()
+  const cosPlayer = await cpModel.findOne({ _id: album.cpID }).exec()
+
+  let imgSet
+
+  for (img of imgList) {
+    const inpURL = img.url.replace('https://', '')
+    const imageURL = `https://cdn.statically.io/img/${inpURL}`
+
+    const template = `<a class="img" style="background: url(${imageURL});" href="/albums/${slug}/${img._id}"></a>`
+
+    if (imgSet == undefined) {
+      imgSet = template
+    } else {
+      imgSet += template
+    }
+  }
+
+  const head = `<title> ${cosPlayer.cpName} ${album.albumName} | ${process.env.web_title}</title>`
+
+  const body = `<div class="mainContainer">
+  <h1 style="margin-top: 1rem; margin-bottom: 1rem;">
+    ${cosPlayer.cpName} - ${album.albumName}
+  </h1>
+  
+  <div class="gallery">
+    ${imgSet}
+  </div>
+</div>`
+
+  return Layout(head, body, 'album')
+}
+
+async function imagePage(albSlug, imgSlug) {
+  const imgList = await imgModel
+    .find({ albumID: albSlug })
+    .sort({ _id: 'desc' })
+    .exec()
+  const album = await abModel.findOne({ _id: albSlug }).exec()
+  const cosPlayer = await cpModel.findOne({ _id: album.cpID }).exec()
+
+  let imageList = []
+
+  for (iimg of imgList) {
+    imageList.push(iimg.url)
+  }
+
+  const searchImg = await imgModel.findOne({ _id: imgSlug }).exec()
+  let state = ['', '']
+
+  if (imageList.indexOf(searchImg.url) == 0) {
+    state[0] = 'hide'
+  } else {
+    state[0] = ''
+  }
+
+  if (imageList.indexOf(searchImg.url) == imageList.length - 1) {
+    state[1] = 'hide'
+  } else {
+    state[1] = ''
+  }
+
+  const prevLink =
+    state[0] != 'hide'
+      ? `/albums/${albSlug}/${
+          (
+            await imgModel
+              .findOne({
+                albumID: albSlug,
+                url: imageList[imageList.indexOf(searchImg.url) - 1],
+              })
+              .exec()
+          )._id
+        }`
+      : `#`
+  const nextLink =
+    state[1] != 'hide'
+      ? `/albums/${albSlug}/${
+          (
+            await imgModel
+              .findOne({
+                albumID: albSlug,
+                url: imageList[imageList.indexOf(searchImg.url) + 1],
+              })
+              .exec()
+          )._id
+        }`
+      : `#`
+
+  const head = `<title> ${cosPlayer.cpName} ${album.albumName} | ${process.env.web_title}</title>`
+
+  const body = `<div class="control-container">
+  <a class="controller ${state[0]}" href="${prevLink}">
+    <h1>
+      &lt;
+    </h1>
+  </a>
+  <div class="image">
+    <img src="${searchImg.url}" alt="${imgSlug}"> 
+  </div>
+  <a class="controller ${state[1]}" href="${nextLink}">
+    <h1>
+      &gt;
+    </h1>
+  </a>
+</div>`
+
+  return Layout(head, body, 'image')
+}
+
+async function albumSlug(app) {
+  app.get('/albums/:album', async (req, res) => {
+    res.send(await albumPage(req.params.album))
+  })
+
+  app.get('/albums/:album/:pic', async (req, res) => {
+    res.send(await imagePage(req.params.album, req.params.pic))
+  })
+}
+
+module.exports = albumSlug
