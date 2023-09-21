33555d34431aad5a820472c45c2bbcc824bbf5a9
Suphakit P <me@suphakit.net>
2023-09-21 15:48:04 +0700

    feat<about>: finalized about page

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,9 +1,27 @@
 const Layout = require('./_Layout')
 
 async function aboutPage() {
-  const head = ``
+  const head = `<title>About | ${process.env.web_title}</title>`
 
-  const body = ``
+  const body = `<div class="mainContainer" style="text-align: center; justify-content: center; margin-top: 4rem;">
+  <h1 style="margin-bottom: 1rem; margin-top: 1rem;">
+    <u>
+      About
+    </u>
+  </h1>
+
+  <p class="text-holder">
+    This is an image CDN management software based on express.js which is making by <a href="https://suphakit.net" target="_blank">Suphakit P.</a> and this project is open-source if you have something that made you crazy and wanna do some improvement, just feel free to send me a pull request :)
+  </p>
+
+  <br>
+
+  <p>
+    <a href="https://github.com/katzEco/katz-gallery" target="_blank">
+      <i class="fa-brands fa-github fa-xl"></i>
+    </a>
+  </p>
+</div>`
 
   return Layout(head, body, 'about')
 }
